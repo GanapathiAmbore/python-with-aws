ff3f2fbe97ab6b4af7dcc63c6d345b96e698ab81
GanapathiAmbore <50941599+GanapathiAmbore@users.noreply.github.com>
2019-08-02 12:22:10 +0530

    Create Data_loading_from_s3.py

---
+++ Data_loading_from_s3.py
@@ -0,0 +1,25 @@
+import boto3
+import re
+import requests
+import json
+from requests_aws4auth import AWS4Auth
+region='us-east-1'
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+host = 'https://search-es-test-domain-jednpfqpvx5t4lxn3xz6pkqtfa.us-east-1.es.amazonaws.com' # the Amazon ES domain, including https://
+index = 'lambda-s3-index'
+type = 'lambda-type'
+url = host + '/' + index + '/' + type
+headers = { "Content-Type": "application/json" }
+s3 = boto3.resource('s3')
+def handler(event,context):
+    bucket_name=event["bucket_name"]
+    folder_name=event["folder_name"]
+    bucket=s3.Bucket(bucket_name)
+    for files in bucket.objects.filter(Prefix=folder_name):
+        file_content = files.get()['Body'].read().decode('utf-8')
+        json_content = json.loads(file_content)
+        lines = file_content.splitlines()
+        r = requests.post(url, auth=awsauth, json=json_content, headers=headers)
+
